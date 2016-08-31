ecf91db2c4e54bf640ea8808d81cfa50ad810b70
MSturges <Sturges3232@gmail.com>
2016-08-31 11:49:36 -0600

    testing godaddy connection

--- public/js/services/HelperServices/HostService.js
+++ public/js/services/HelperServices/HostService.js
@@ -9,6 +9,7 @@
   ];
 
   function HostService () {
+    console.log('CLIENTSIDE HOLMES...');
     var hostArr = window.location.origin.split(':');
     this.api = hostArr[hostArr.length-1] === '8080' ? 'http://localhost:3000' : 'https://crushroutesapi.herokuapp.com';
   }
